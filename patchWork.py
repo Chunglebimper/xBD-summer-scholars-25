fbfd44bb1cfa18b517bfa60e007809adfa9218ce
Chunglebimper <chunglebimper@protonmail.com>
2025-05-27 11:28:50 -0400

    outputs tifs not jpg

--- patchWork.py
+++ patchWork.py
@@ -14,7 +14,7 @@ def display(PATH):
     plt.tight_layout()
     # show the image
     plt.axis('off')
-    plt.savefig('output.jpg', transparent=True, dpi=300, bbox_inches="tight", pad_inches=0.0)   # save the image
+    plt.savefig('output.tif', transparent=True, dpi=300, bbox_inches="tight", pad_inches=0.0)   # save the image
     plt.savefig('output2.tif', transparent=True, dpi=300, bbox_inches="tight", pad_inches=0.0)
 
 
@@ -26,6 +26,6 @@ def display(PATH):
 GEO_PATH = "/home/crota/Pictures/geotiffs/tier3/images/joplin-tornado_00000000_post_disaster.tif" # change to image location
 
 display(GEO_PATH)
-splits = split_image.split_image('output.jpg', 8, 8, False, True, output_dir='./processed')
+splits = split_image.split_image('output.tif', 8, 8, False, True, output_dir='./processed')
 
 #print(skimage.io.imread((f'{GEO_PATH}/joplin-tornado_00000000_post_disaster.tif'))) #displays rgb values
